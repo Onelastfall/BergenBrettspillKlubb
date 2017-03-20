b7aa5ac809462a85f261d20e11489aef2f0ca527
INF112v16-yst003 <yijun@staut.land>
2017-03-20 08:52:06 +0100

    Kjørte et par test ting.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BoardgameComponent } from './boardgame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardgameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/boardgame.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component
+({
+    selector: 'boardgame',
+    template: '<h1> Brettspill! </h1>'
+})
+
+export class BoardgameComponent
+{
+
+}
+
